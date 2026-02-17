2c17e74f4e8b00ada2fbce81cbb018cf6b181633
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-17 12:23:15 +0100

    HeroSection

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Features } from './components/features/features';
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Hero, Features, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
