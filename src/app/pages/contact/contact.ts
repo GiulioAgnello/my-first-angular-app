6cac15b7ab490c2dba66e0b87a9963c690bf907a
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-18 17:09:20 +0100

    contactPage

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -7,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './contact.css',
 })
 export class Contact {
-
+  contacts = [
+    {
+      id: 1,
+      logo: './assets/email.png',
+      label: 'Email',
+      value: 'agnellogiulio@yahoo.com',
+      href: 'mailto:agnellogiulio@yahoo.com',
+    },
+    {
+      id: 2,
+      icon: 'WhatsApp',
+      label: 'WhatsApp',
+      value: '+39 3271208496',
+      href: 'https://wa.me/393271208496',
+    },
+    {
+      id: 3,
+      icon: 'GitHub',
+      label: 'GitHub',
+      value: '@GiulioAgnello',
+      href: 'https://github.com/GiulioAgnello',
+    },
+    {
+      id: 4,
+      icon: 'LinkedIn',
+      label: 'LinkedIn',
+      value: 'Giulio Agnello',
+      href: 'https://www.linkedin.com/in/giulio-agnello/',
+    },
+    {
+      id: 5,
+      icon: 'Instagram',
+      label: 'Instagram',
+      value: '@giulioagnello',
+      href: 'https://www.instagram.com/giulioagnello?igsh=d2R1aWZ6aHd0bTUw&utm_source=qr',
+    },
+    {
+      id: 6,
+      icon: 'Facebook',
+      label: 'Facebook',
+      value: '@giulio.agnello.1',
+      href: 'https://www.facebook.com/giulio.agnello.1/',
+    },
+  ];
 }
