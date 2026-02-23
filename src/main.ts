4263dc8c84db27f1d2f86b5bd50df624f9a0c226
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-23 22:17:19 +0100

    online

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import '@angular/compiler';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
